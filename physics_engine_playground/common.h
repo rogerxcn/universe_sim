3e23c9ddcb4b4e2b7d803e01c1b2260d03625e66
Roger Xin <admin@rogerx.me>
2018-04-22 17:05:25 -0400

    cleanup

--- physics_engine_playground/common.h
+++
@@ -1,8 +0,0 @@
-#ifndef COMMON_H_
-#define COMMON_H_
-
-#include <iostream>
-#include <string>
-#include <vector>
-
-#endif
--- physics_engine_playground/system.cpp
+++
@@ -1,9 +0,0 @@
-#include "system.h"
-
-void System::setName(std::string n) { name = n; }
-
-void System::addStar(Star* s) { stars.push_back(s); }
-
-void System::addPlanet(Planet* p) { planets.push_back(p); }
-
-void System::addMovingObject(MovingObject* m) { mos.push_back(m); }
